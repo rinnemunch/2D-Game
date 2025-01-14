1e661a09090c19779cb58d731f5b96f21504bd4e
Shaun Fulton <fulton_shaun@yahoo.com>
2025-01-14 18:23:50 -0500

    Added horizontal inbound system for our player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,7 +12,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //take the current position = new position (0,0,0)
+
         transform.position = new Vector3(0,0,0);
     }
 
@@ -22,12 +22,29 @@ public class Player : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
-        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
 
         Vector3 direction = new Vector3 (horizontalInput, verticalInput, 0);
 
         transform.Translate(direction * _speed * Time.deltaTime);
+
+        //if player position on the y is greater than 0  
+        // y position = 0
+        //else if position on the y is less than -3.8f 
+        //y pos = -3.8f 
         
+        if (transform.position.y >= 0)
+        {
+            transform.position = new Vector3(transform.position.x,0,0);
+        }
+        else if (transform.position.y <= -3.8f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.8f, 0); 
+        }
+
+        //if player on the x > 11 
+        // x pos = -11 
+        //else if player on the x is less than -11 
+        //x pos = 11 
+
     }
 }
