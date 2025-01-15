c5f32ea209d93e6858e1823fce366c73ea3a23b7
Shaun Fulton <fulton_shaun@yahoo.com>
2025-01-14 19:16:11 -0500

    Optimized and cleaned up the code

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,41 +19,28 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
 
-        Vector3 direction = new Vector3 (horizontalInput, verticalInput, 0);
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
         transform.Translate(direction * _speed * Time.deltaTime);
 
-        //if player position on the y is greater than 0  
-        // y position = 0
-        //else if position on the y is less than -3.8f 
-        //y pos = -3.8f 
-        
-        if (transform.position.y >= 0)
-        {
-            transform.position = new Vector3(transform.position.x,0,0);
-        }
-        else if (transform.position.y <= -3.8f)
-        {
-            transform.position = new Vector3(transform.position.x, -3.8f, 0); 
-        }
-
-        //if player on the x > 11 
-        // x pos = -11 
-        //else if player on the x is less than -11 
-        //x pos = 11 
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
 
         if (transform.position.x > 11.3f)
         {
-            transform.position = new Vector3(11.3f, transform.position.y, 0);
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
         }
         else if (transform.position.x < -11.3f)
         {
-            transform.position = new Vector3(11.3f, transform.position.y, 0); 
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
         }
-
     }
 }
