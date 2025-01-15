2130de7fbc8edc8ea63569f7e5c418e0dc189b0a
Shaun Fulton <fulton_shaun@yahoo.com>
2025-01-14 19:27:38 -0500

    Finalized player character

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,39 +12,34 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         transform.position = new Vector3(0,0,0);
     }
 
     // Update is called once per frame
     void Update()
     {
+   
+        CalculateMovement();    
+    }
+    void CalculateMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
 
-        Vector3 direction = new Vector3 (horizontalInput, verticalInput, 0);
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
 
         transform.Translate(direction * _speed * Time.deltaTime);
 
-        //if player position on the y is greater than 0  
-        // y position = 0
-        //else if position on the y is less than -3.8f 
-        //y pos = -3.8f 
-        
-        if (transform.position.y >= 0)
-        {
-            transform.position = new Vector3(transform.position.x,0,0);
-        }
-        else if (transform.position.y <= -3.8f)
-        {
-            transform.position = new Vector3(transform.position.x, -3.8f, 0); 
-        }
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0); 
 
-        //if player on the x > 11 
-        // x pos = -11 
-        //else if player on the x is less than -11 
-        //x pos = 11 
-
+        if (transform.position.x > 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0); 
+        }
+        else if (transform.position.x < -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
     }
 }
