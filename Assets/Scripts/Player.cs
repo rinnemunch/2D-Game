45fcd290d9d0af8237f3721541b17e1cc0ba9853
Shaun Fulton <fulton_shaun@yahoo.com>
2025-01-14 23:33:40 -0500

    added cooldown system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,9 @@ public class Player : MonoBehaviour
     private float _speed = 3.5f;
     [SerializeField]
     private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _canFire = -1f; 
 
 
 
@@ -26,8 +29,9 @@ public class Player : MonoBehaviour
         //if i hit the space key 
         //spawn gameObject 
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
         {
+            _canFire = Time.time + _fireRate;
             Instantiate(_laserPrefab, transform.position + new Vector3(0, 0.8f, 0), Quaternion.identity);
         }
     }
