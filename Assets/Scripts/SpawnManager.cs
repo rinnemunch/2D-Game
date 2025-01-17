a474094795cbe472c561517f2ae14fff3f09e639
Shaun Fulton <fulton_shaun@yahoo.com>
2025-01-16 19:02:44 -0500

    Added spawn routine

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject _enemyPrefab;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(SpawnRountine()); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    //spawn game objects every 5 seconds
+    //Create a coroutine of type IEnumerator -- Yield Events 
+    //while loop 
+
+    IEnumerator SpawnRountine()
+    {
+        while (true)
+        {
+            Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0); 
+            Instantiate( _enemyPrefab, posToSpawn, Quaternion.identity);
+            yield return new WaitForSeconds(5.0f);
+        }
+
     }
 }
