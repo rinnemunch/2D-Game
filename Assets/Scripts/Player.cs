97b7a45312e29971ac59807528be4d66f2aee3ba
Shaun Fulton <fulton_shaun@yahoo.com>
2025-01-23 22:56:51 -0500

    Fixed fire offset, added triple fire behavior

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,11 +9,18 @@ public class Player : MonoBehaviour
     [SerializeField]
     private GameObject _laserPrefab;
     [SerializeField]
+    private GameObject _tripleShotPrefab;
+    [SerializeField]
     private float _fireRate = 0.5f;
     private float _canFire = -1f;
     [SerializeField]
     private int _lives = 3;
-    private SpawnManager _spawnManager; 
+    private SpawnManager _spawnManager;
+
+    [SerializeField]
+    private bool _isTripleShotActive = false;
+
+    //variable for isTripleShotActive 
 
 
 
@@ -65,7 +72,16 @@ public class Player : MonoBehaviour
     void FireLaser()
     {
         _canFire = Time.time + _fireRate;
-        Instantiate(_laserPrefab, transform.position + new Vector3(0, 1.05f, 0), Quaternion.identity);
+
+        if (_isTripleShotActive == true)
+        {
+            Instantiate(_tripleShotPrefab, transform.position + new Vector3(0, 1.0f, 0), Quaternion.identity);
+        }
+
+        else
+        {
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 1.05f, 0), Quaternion.identity);
+        }
     }
 
     public void Damage()
