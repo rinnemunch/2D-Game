4a6481dd76f970d22f368fa9d980adccc9df3a57
Shaun Fulton <fulton_shaun@yahoo.com>
2025-01-25 18:18:49 -0500

    Asteroid added along with rotational behavior, explosion for it added

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    private float _rotateSpeed = 3.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * _rotateSpeed *  Time.deltaTime);
+    }
+}
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,11 +8,24 @@ public class Enemy : MonoBehaviour
     private float _speed = 4.0f;
 
     private Player _player;
+    private Animator _anim; 
 
     // Start is called before the first frame update
     void Start()
     {
         _player = GameObject.Find("Player").GetComponent<Player>();
+        //null check player 
+        if (_player == null )
+        {
+            Debug.LogError("The Player is NULL.");
+        }
+        
+        _anim = GetComponent<Animator>();
+
+        if (_anim == null )
+        {
+            Debug.LogError("The animator is NULL."); 
+        }
     }
 
 
@@ -39,8 +52,9 @@ public class Enemy : MonoBehaviour
             {
                 player.Damage();
             }
-
-            Destroy(this.gameObject);
+            _anim.SetTrigger("OnEnemyDeath");
+            _speed = 0;
+            Destroy(this.gameObject, 2.8f);
         }
 
 
@@ -52,8 +66,9 @@ public class Enemy : MonoBehaviour
             {
                 _player.AddScore(10);
             }
-
-            Destroy(this.gameObject);
+            _anim.SetTrigger("OnEnemyDeath"); 
+            _speed = 0.5f;
+            Destroy(this.gameObject, 2.8f);
         }
     }
 }
