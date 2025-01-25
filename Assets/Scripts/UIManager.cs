6559ad74ece55873c0922a17f093735738261165
Shaun Fulton <fulton_shaun@yahoo.com>
2025-01-25 00:34:01 -0500

    You can now click 'r' to restart the game

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -13,11 +13,21 @@ public class UIManager : MonoBehaviour
     private Sprite[] _liveSprites;
     [SerializeField]
     private Text _gameOverText;
+    [SerializeField]
+    private Text _restartText;
+
+    private GameManager _gameManager;
     
     void Start()
     {
         _scoreText.text = "Score: " + 0; 
         _gameOverText.gameObject.SetActive(false);
+        _gameManager = GameObject.Find("Game_Manager").GetComponent<GameManager>();   
+
+        if (_gameManager != null )
+        {
+            Debug.LogError("GameManager is NULL.");
+        }
     }
 
     public void UpdateScore(int playerScore)
@@ -31,7 +41,26 @@ public class UIManager : MonoBehaviour
 
         if (currentLives == 0)
         {
-            _gameOverText.gameObject.SetActive(true);
+            GameOverSequence();
+        }
+    }
+
+    void GameOverSequence()
+    {
+        _gameManager.GameOver();
+        _gameOverText.gameObject.SetActive(true);
+        _restartText.gameObject.SetActive(true);   
+        StartCoroutine(GameOverFlickerRoutine());
+    }
+
+    IEnumerator GameOverFlickerRoutine()
+    {
+        while(true)
+        {
+            _gameOverText.text = "GAME OVER"; 
+            yield return new WaitForSeconds(0.5f);
+            _gameOverText.text = "";
+            yield return new WaitForSeconds(0.5f);
         }
     }
 }
