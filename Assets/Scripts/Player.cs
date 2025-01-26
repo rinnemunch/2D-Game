c3f847fa38f9646a81521a07dd2d346214f7be06
Shaun Fulton <fulton_shaun@yahoo.com>
2025-01-25 19:24:07 -0500

    Added player damage visualization and animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,6 +24,8 @@ public class Player : MonoBehaviour
 
     [SerializeField]
     private GameObject _shieldVisualizer;
+    [SerializeField]
+    private GameObject _leftEngine, _rightEngine;
 
     [SerializeField]
     private int _score;
@@ -107,10 +109,6 @@ public class Player : MonoBehaviour
 
     public void Damage()
     {
-        //if shields is active 
-        //do nothing...
-        //deactivate shields
-        //return; 
         if (_isShieldsActive == true)
         {
             _isShieldsActive = false;
@@ -119,6 +117,15 @@ public class Player : MonoBehaviour
         } 
 
         _lives--;
+
+        if (_lives == 2)
+        {
+            _leftEngine.SetActive(true);
+        }
+        else if (_lives == 1)
+        {
+            _rightEngine.SetActive(true);
+        }
 
         _uiManager.UpdateLives(_lives); 
 
