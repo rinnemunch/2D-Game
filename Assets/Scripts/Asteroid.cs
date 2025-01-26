248f3c4b2bc35d812a51199aa4043e56e0c7a219
Shaun Fulton <fulton_shaun@yahoo.com>
2025-01-25 19:07:15 -0500

    Asteriod Spawn Wave

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -6,15 +6,33 @@ public class Asteroid : MonoBehaviour
 {
     [SerializeField]
     private float _rotateSpeed = 3.0f;
-    // Start is called before the first frame update
+    [SerializeField]
+    private GameObject _explosionPrefab;
+    private SpawnManager _spawnManager;
+
+ 
+
     void Start()
     {
-        
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Rotate(Vector3.forward * _rotateSpeed *  Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+       if (other.tag == "Laser")
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            Destroy(other.gameObject);
+
+            _spawnManager.StartSpawning();
+
+            Destroy(this.gameObject, 0.25f);
+        }
     }
 }
--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -17,6 +17,11 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+
+    }
+    
+    public void StartSpawning()
+    {
         StartCoroutine(SpawnEnemyRountine());
         StartCoroutine(SpawnPowerupRoutine());
     }
@@ -31,6 +36,7 @@ public class SpawnManager : MonoBehaviour
 
     IEnumerator SpawnEnemyRountine()
     {
+        yield return new WaitForSeconds(3.0f);
         while (_stopSpawning == false)
         {
             Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
@@ -43,6 +49,7 @@ public class SpawnManager : MonoBehaviour
 
     IEnumerator SpawnPowerupRoutine()
     {
+        yield return new WaitForSeconds(3.0f);
         while (_stopSpawning == false)
         {
             Vector3 posToSpawn = new Vector3(Random.Range(-8f, 8f), 7, 0);
